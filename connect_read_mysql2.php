ee72e8748bb6c34df279d1adb166a44068324833
nocane <rmarsh00@gmail.com>
2020-06-16 16:34:48 +0100

    PHP Read from MySQL4

--- connect_read_mysql2.php
+++ connect_read_mysql2.php
@@ -27,7 +27,7 @@ if (!$link) {
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-$result = mysqli_query($link,"SELECT * FROM Persons");
+$result = mysqli_query($link,"SELECT * FROM cocktails.cocktail_details");
 
 echo "<table border='1'>
 <tr>
